7b3be1ba76d9d32687cb26b09ba2f609d2e9a6c4
Dmitrii Shchadei <gmetrofun@gmail.com>
2013-09-08 21:21:10 +0300

    gh-pages config

--- modules/app/app.js
+++ modules/app/app.js
@@ -3,9 +3,9 @@
  */
 require(['playlist', 'header-search'], function (Playlist) {
     SC.initialize({
-        client_id: 'f9ff3ee51fed2d2e0bc09d60d4e00896',
+        client_id: '3136aa18c7383fa2da56f6ed33e8e8d0',
         display: 'popup',
-        redirect_uri: 'http://127.0.0.1:8000/callback.html'
+        redirect_uri: 'http://metrofun.github.io/web-dev-challenge/callback.html'
     });
     SC.connect(function () {
         Playlist.html(function (html) {
